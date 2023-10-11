40d04cb2336b8d986e82b7e503fbb0e9d7018058
JonathanDavidAntonio <antoniojonathan71@gmail.com>
2023-10-11 15:36:25 +0800

    created signup page

---
+++ front-end/src/pages/SignupPage.js
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+export const SignupPage = () => {
+    const [errorMessage, setErrorMessage] = useState('');
+    const [emailValue, setEmailValue] =  useState('');
+    const [passwordValue, setPasswordValue] = useState('');
+    const [confirmPasswordValue, setConfirmPasswordValue] = useState('');
+
+    const history = useHistory();
+
+    const onSignupClicked = async () => {
+        alert('Sign Up Not implement yet');
+    };
+
+    return (
+        <div className='content-container'>
+            <h1>Sign Up</h1>
+            {errorMessage && <div className='fail'>{errorMessage}</div>}
+            <input 
+                value= {emailValue}
+                onChange={e => setEmailValue(e.target.value)}
+                placeholder="someone@gmail.com">
+            </input>
+            <input
+                type="password"
+                value={passwordValue}
+                onChange={e => setPasswordValue(e.target.value)}
+                placeholder="Your password">
+            </input>
+            <input
+                type="password"
+                value={confirmPasswordValue}
+                onChange={e => setConfirmPasswordValue(e.target.value)}
+                placeholder="Confirm your password">
+            </input>
+            <button 
+                disabled={
+                    !emailValue || !passwordValue ||
+                    passwordValue !== confirmPasswordValue
+                }
+                onClick={onSignupClicked}>Sign Up</button>
+            <button
+                onClick={() => history.push('/login')}>Already have an account? Log In</button>
+        </div>
+    );
+}
