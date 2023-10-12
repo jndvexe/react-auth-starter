f0957d999ccaac5c0518beb6f527b6b419cba141
JonathanDavidAntonio <antoniojonathan71@gmail.com>
2023-10-12 17:34:58 +0800

    adding jwt to user info page

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -2,9 +2,11 @@ import { sign } from 'jsonwebtoken';
 import { signUpRoute } from './signUpRoute';
 import { logInRoute } from './logInRoute';
 import { testRoute } from './testRoute';
+import { updateUserInfoRoute } from './updateUserInfoRoute';
 
 export const routes = [
     logInRoute,
     signUpRoute,
     testRoute,
+    updateUserInfoRoute,
 ];
--- back-end/src/routes/updateUserInfoRoute.js
+++ back-end/src/routes/updateUserInfoRoute.js
@@ -1,15 +1,15 @@
-import { Jwt } from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 import { ObjectID } from 'mongodb';
-import { getDbConnection } from "../db";
+import { getDbConnection } from '../db';
 
 export const updateUserInfoRoute = {
-    path:'/api/users/:userId',
+    path: '/api/users/:userId',
     method: 'put',
     handler: async (req, res) => {
         const { authorization } = req.headers;
         const { userId } = req.params;
 
-        const updates = ({
+        const updates = (({
             favoriteFood,
             hairColor,
             bio,
@@ -17,12 +17,35 @@ export const updateUserInfoRoute = {
             favoriteFood,
             hairColor,
             bio,
-        }) (req.body);
+        }))(req.body);
 
         if (!authorization) {
-            return res.status(401).json({message: 'No authorization sent'})
+            return res.status(401).json({ message: 'No authorization header sent' });
         }
 
         const token = authorization.split(' ')[1];
+
+        jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+            if (err) return res.status(401).json({ message: 'Unable to verify token' });
+
+            const { id } = decoded;
+
+            if (id !== userId) return res.status(403).json({ message: 'Not allowed to update that user\'s data' });
+
+            const db = getDbConnection('react-auth-db');
+            const result = await db.collection('users').findOneAndUpdate(
+                { _id: ObjectID(id) },
+                { $set: { info: updates } },
+                { returnOriginal: false },
+            );
+            const { email, isVerified, info } = result.value;
+
+            jwt.sign({ id, email, isVerified, info }, process.env.JWT_SECRET, { expiresIn: '2d' }, (err, token) => {
+                if (err) {
+                    return res.status(200).json(err);
+                }
+                res.status(200).json({ token });
+            });
+        })
     }
 }
