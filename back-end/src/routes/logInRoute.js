b046f3be5db144c6a52c36d3e9c21742e49ae7bf
JonathanDavidAntonio <antoniojonathan71@gmail.com>
2023-10-12 17:07:19 +0800

    adding update user route

--- back-end/src/routes/logInRoute.js
+++ back-end/src/routes/logInRoute.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import { getDbConnection } from '../db';;
+import { getDbConnection } from '../db';
 
 export const logInRoute = {
     path: '/api/login',
@@ -11,20 +11,22 @@ export const logInRoute = {
         const db = getDbConnection('react-auth-db');
         const user = await db.collection('users').findOne({ email });
 
-        if (!user) return res.sendStatus(400);
+        if (!user) return res.sendStatus(401);
 
         const { _id: id, isVerified, passwordHash, info } = user;
+
         const isCorrect = await bcrypt.compare(password, passwordHash);
 
         if (isCorrect) {
-            jwt.sign({ id, isVerified, email, info}, process.env.JWT_SECRET, { expiresIn:'2d'}, (err, token) => {
-                if (err){
-                    res.status(500);
+            jwt.sign({ id, isVerified, email, info }, process.env.JWT_SECRET, { expiresIn: '2d' }, (err, token) => {
+                if (err) {
+                    res.status(500).json(err);
                 }
-                res.sendStatus(200).json({token});
-            })
+
+                res.status(200).json({ token });
+            });
         } else {
             res.sendStatus(401);
         }
-    }
+    },
 }
